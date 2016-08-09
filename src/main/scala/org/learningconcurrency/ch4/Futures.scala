82f5949b3128c8b07a833888e71d765b65324c02
tophua <tophua@gmail.com>
2016-08-10 06:24:43 +0800

    FuturesCallbacks 修改方法,增加Thread.sleep方法

--- src/main/scala/org/learningconcurrency/ch4/Futures.scala
+++ src/main/scala/org/learningconcurrency/ch4/Futures.scala
@@ -8,7 +8,13 @@ import org.learningconcurrency._
  *  针对这个结果可以添加 Callback 以便在任务执行成功或失败后做出对应的操作
  */
 object FuturesComputation extends App {
-  //Computation计算
+  /**
+   * Computation计算
+   *  Futures 执行计算
+   * 1,首先引入global执行上下文,可以确保在全局上下文中执行Future计算
+   * 调用log方法的次序不是确定,后跟代码块的Futures单例对象,是为调用Apply方法而添加语法糖
+   * 
+   */
   import scala.concurrent._
   import ExecutionContext.Implicits.global
 
@@ -21,52 +27,108 @@ object FuturesComputation extends App {
 }
 
 /**
- * 数据类型
+ *  通过Futures计算,使用Source.fromFile对象读取build.sbt文件内容
+ * 1,首先引入global执行上下文,可以确保在全局上下文中执行Future计算
+ * 
+ * 
  */
 object FuturesDataType extends App {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
   import scala.io.Source
-
+ //通过Futures计算,使用Source.fromFile对象读取build.sbt文件内容
   val buildFile: Future[String] = Future {
     val f = Source.fromFile("build.sbt")
     try f.getLines.mkString("\n") finally f.close()
   }
 
   log(s"started reading build file asynchronously")
+  /**
+   * main线程会调用Future对象中的isCompleted方法,该Future对象为通过执行Future计算获得buildFile对象
+   * 读取build.sbt文件的操作很可能很快完成,因此isCompleted方法会返回false,
+   * 过250毫秒后,main线程会再次调用isCompleted方法会返回true,
+   * 最后main线程会调用value方法,该方法会返回build.sb文件的内容
+   * 
+   */
   log(s"status: ${buildFile.isCompleted}")//是否完成
   Thread.sleep(250)
   log(s"status: ${buildFile.isCompleted}")//是否完成
   log(s"status: ${buildFile.value}")//还回值
 
 }
-
+/**
+ * Futures对象的回调函数
+ * 从w3网中查找出所有单词telnet.
+ */
 
 object FuturesCallbacks extends App {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
   import scala.io.Source
-
-  def getUrlSpec(): Future[Seq[String]] = Future {
+/**
+ * 从w3网站获得url规范的文档,使用Source对象存储该文件的内容,并使用getUrlSpec文件中的Future对象
+ * 以异步方式执行Http请求操作,getUrlSpec方法会先调用fromURL获取含有文本文档的Source对象,然后会调用
+ * getLines方法获取文档中的行列表.
+ */
+  
+  def getUrlSpec(): Future[Seq[String]] = Future {    
     val f = Source.fromURL("http://www.w3.org/Addressing/URL/url-spec.txt")
-    try f.getLines.toList finally f.close()
+    try {
+      f.getLines.toList
+      }finally{
+      f.close()
+     }
   }
 
   val urlSpec: Future[Seq[String]] = getUrlSpec()
-
-  def find(lines: Seq[String], word: String) = lines.zipWithIndex collect {
+/**
+ * 要在Future对象urlSpec中找到包含有单词telnet的行,可以使用find方法
+ * 该方法将行的列表和需要搜索的单词接收为参数,并且返回含有匹配内容的字符串.
+ * 接收一个Seq类型的参数,而urlSpec对象返回Future[Seq[String]],因此无法将Future对象urlSpec
+ * 直接发送给find方法,而且在程序调用find方法时,该Future很可能还无法使用.
+ */
+  def find(lines: Seq[String], word: String) = lines.zipWithIndex.collect {
     case (line, n) if line.contains(word) => (n, line)
-  } mkString("\n")
-
-  urlSpec foreach {
+  }.mkString("\n")
+/**
+ * 我们使用foreach方法为这个Future添加一个回调函数,注意onSuccess方法与foreach方法等价,但onSuccess方法可能
+ * 会在scala 2.11之后被弃用,foreach方法接收偏函数作为其参数
+ * 此处的要点是:添加回调函数是非阻塞操作,回调用函数注册后,main线程中用的log语句会立即执行
+ * 但是执行回调函数中log语句的时间可以晚得多
+ * 在Future对象被完善后,无须立刻调用回调参数,大多数执行上下文通过调用任务,以异步方式处理回调函数
+ */
+  urlSpec.foreach {
     lines => log(s"Found occurrences of 'telnet'\n${find(lines, "telnet")}\n")
   }
+   Thread.sleep(2000)
+   log("callbacks registered, continuing with other work")
 
-  urlSpec foreach {
+/**
+ * ForkJoinPool-1-worker-5: Found occurrences of 'telnet'
+ * (207,  telnet , rlogin and tn3270 )
+ * (745,                         nntpaddress | prosperoaddress | telnetaddress)
+ * (806,  telnetaddress           t e l n e t : / / login )
+ * (931,   for a given protocol (for example,  CR and LF characters for telnet)
+   ForkJoinPool-1-worker-5: Found occurrences of 'password'
+ * (107,                         servers). The password, is present, follows)
+ * (109,                         the user name and optional password are)
+ * (111,                         user of user name and passwords which are)
+ * (222,      User name and password)
+ * (225,   password for those systems which do not use the anonymous FTP)
+ * (226,   convention. The default, however, if no user or password is)
+ * (234,   is "anonymous" and the password the user's Internet-style mail)
+ * (240,   currently vary in their treatment of the anonymous password.  )
+ * (816,  login                   [ user [ : password ] @ ] hostport )
+ * (844,  password                alphanum2 [ password ] )
+ * (938,   The use of URLs containing passwords is clearly unwise. )
+ */
+  urlSpec.foreach {
     lines => log(s"Found occurrences of 'password'\n${find(lines, "password")}\n")
   }
+  Thread.sleep(1000)
 
   log("callbacks installed, continuing with other work")
+  
 
 }
 
